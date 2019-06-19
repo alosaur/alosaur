eefd7d32a7d786591c12aea07f27bd66f06d4491
irustm <jamakrus@gmail.com>
2019-06-19 20:42:13 +0300

    update deps, remove dejs

--- src/renderer/view.ts
+++ src/renderer/view.ts
@@ -1,6 +1,8 @@
-import { renderFile, contentType, Response } from '../package.ts';
+// import { renderFile, contentType, Response } from '../package.ts';
+import { contentType, Response } from '../package.ts';
 import { normalize } from '../package.ts';
 import { getViewRenderConfig } from '../mod.ts';
+import { MethodNotAllowedError } from '../http-error/MethodNotAllowedError.ts';
 
 export async function View(templatePath: string, model: Object, status: number = 200): Promise<Response> {
     let body;
@@ -8,7 +10,8 @@ export async function View(templatePath: string, model: Object, status: number =
     headers.set("content-type", contentType("text/html"));
     const renderConfig = getViewRenderConfig();
     // if renderConfig.type === dejs
-    body = await renderFile(normalize(`${renderConfig.basePath}${templatePath}.ejs`), model);;
+    throw new MethodNotAllowedError('waiting this PR: https://github.com/syumai/dejs/pull/21')
+    // body = await renderFile(normalize(`${renderConfig.basePath}${templatePath}.ejs`), model);;
     
     return {
       body,
