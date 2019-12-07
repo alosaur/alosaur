4575f1c6be6488ffeb07b3b2d52809ff5e07b463
irustm <jamakrus@gmail.com>
2019-12-07 22:57:24 +0300

    Up to v.0.26

--- src/package_test.ts
+++ src/package_test.ts
@@ -1,4 +1,4 @@
-export { test, runTests } from "https://deno.land/std@v0.24.0/testing/mod.ts";
+export { test, runTests } from "https://deno.land/std@v0.26.0/testing/mod.ts";
 
 export {
   assert,
@@ -6,4 +6,4 @@ export {
   assertStrictEq,
   assertThrows,
   assertThrowsAsync
-} from "https://deno.land/std@v0.24.0/testing/asserts.ts";
+} from "https://deno.land/std@v0.26.0/testing/asserts.ts";
