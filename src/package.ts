a2a69c931c963c69121e8d581eb569a44085854e
irustm <jamakrus@gmail.com>
2020-02-21 23:51:32 +0300

    Up to v0.34.0

--- src/package.ts
+++ src/package.ts
@@ -3,7 +3,7 @@ export {
   ServerRequest,
   ServerRequestBody,
   Response
-} from "https://deno.land/std@v0.30.0/http/server.ts";
+} from "https://deno.land/std@v0.34.0/http/server.ts";
 export {
   normalize,
   basename,
@@ -13,9 +13,9 @@ export {
   join,
   resolve,
   isAbsolute
-} from "https://deno.land/std@v0.30.0/path/mod.ts";
+} from "https://deno.land/std@v0.34.0/path/mod.ts";
 
-export { getCookies } from "https://deno.land/std@v0.30.0/http/cookie.ts";
-export { contentType } from "https://deno.land/std@v0.30.0/media_types/mod.ts";
+export { getCookies } from "https://deno.land/std@v0.34.0/http/cookie.ts";
+export { contentType } from "https://deno.land/std@v0.34.0/media_types/mod.ts";
 
 export { renderFile } from "https://deno.land/x/dejs@0.3.3/mod.ts";
