a18f1871f9e3c53ac2a17d49a2ceffa8d53894e7
Sean Wong <seanwong24@live.com>
2020-04-17 19:43:43 +0000

    make Content() handles number, boolean, and undefined

--- src/renderer/content.ts
+++ src/renderer/content.ts
@@ -1,27 +1,29 @@
 import { contentType, Response } from "../package.ts";
 
 /**
- * Render json or string content
+ * Render json, number, boolean, or string content
  */
-export function Content(result: string | Object, status: number = 200): Response {
-    let body;
-    const headers = new Headers();
+export function Content(result?: string | number | boolean | Object, status: number = 200): Response {
+  let body;
+  const headers = new Headers();
 
-    switch (typeof result) {
-      case "object":
-          headers.set("content-type", contentType("file.json") as string);
-          body = new TextEncoder().encode(JSON.stringify(result));
-        break;
-        
-      default:
-        headers.set("content-type", contentType("text/html") as string);
-        body = new TextEncoder().encode(result);
-        break;
-    }
-    
-    return {
-      body,
-      status,
-      headers
-    }
+  switch (typeof result) {
+    case "object":
+    case "boolean":
+    case "number":
+      headers.set("content-type", contentType("file.json") as string);
+      body = new TextEncoder().encode(JSON.stringify(result));
+      break;
+
+    default:
+      headers.set("content-type", contentType("text/html") as string);
+      body = new TextEncoder().encode(result || "");
+      break;
+  }
+
+  return {
+    body,
+    status,
+    headers
+  }
 }
