891ac5af13d01494ce64ce4d52f4bba9b4959aa7
irustm <jamakrus@gmail.com>
2020-04-30 11:44:50 +0300

    fixes typings

--- src/middlewares/cors-builder.test.ts
+++ src/middlewares/cors-builder.test.ts
@@ -1,6 +1,6 @@
 import { assert } from "../package_test.ts";
 import { CorsBuilder } from "./cors-builder.ts";
-import { ServerRequest, Response } from "../mod.ts";
+import { ServerRequest, Response, RenderResult } from "../mod.ts";
 const { test } = Deno;
 
 test(function testCorsBuilder() {
@@ -13,7 +13,7 @@ test(function testCorsBuilder() {
     .WithHeaders('X-Custom-Header, Upgrade-Insecure-Requests')
     .AllowCredentials();
 
-  builder.onPostRequest({} as ServerRequest, response, response).then(() => {
+  builder.onPostRequest({} as ServerRequest, response, response as RenderResult).then(() => {
     assert(response && response.headers && response.headers.get('Access-Control-Allow-Origin') === 'http://localhost:8000');
     assert(response && response.headers && response.headers.get('Access-Control-Allow-Methods') === 'PUT, OPTIONS');
     assert(response && response.headers && response.headers.get('Access-Control-Allow-Headers') === 'X-Custom-Header, Upgrade-Insecure-Requests');
