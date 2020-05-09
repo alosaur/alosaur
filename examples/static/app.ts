66d6e10929c50da6af3de9985a870bcaa788c032
NathanSMB <nathan@burchill.io>
2020-05-09 19:10:45 -0400

    Update std to 0.50.0

--- examples/static/app.ts
+++ examples/static/app.ts
@@ -1,5 +1,5 @@
-import { Cookie, setCookie, getCookies } from 'https://deno.land/std@v1.0.0-rc1/http/cookie.ts';
-import { Response } from 'https://deno.land/std@v1.0.0-rc1/http/server.ts';
+import { Cookie, setCookie, getCookies } from 'https://deno.land/std@0.50.0/http/cookie.ts';
+import { Response } from 'https://deno.land/std@0.50.0/http/server.ts';
 import { App, Area, Controller, Get, Req, Res, ServerRequest } from '../../src/mod.ts';
 import { Redirect } from '../../src/renderer/redirect.ts';
 
--- src/package.ts
+++ src/package.ts
@@ -1,4 +1,4 @@
-export { serve, Server, ServerRequest, Response } from 'https://deno.land/std@v1.0.0-rc1/http/server.ts';
+export { serve, Server, ServerRequest, Response } from 'https://deno.land/std@0.50.0/http/server.ts';
 export {
     normalize,
     basename,
@@ -8,7 +8,7 @@ export {
     join,
     resolve,
     isAbsolute,
-} from 'https://deno.land/std@v1.0.0-rc1/path/mod.ts';
+} from 'https://deno.land/std@0.50.0/path/mod.ts';
 
-export { getCookies } from 'https://deno.land/std@v1.0.0-rc1/http/cookie.ts';
-export { contentType } from 'https://deno.land/x/media_types@v2.0.1/mod.ts';
+export { getCookies } from 'https://deno.land/std@0.50.0/http/cookie.ts';
+export { contentType } from 'https://deno.land/x/media_types@v2.2.0/mod.ts';
