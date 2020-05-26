b8aa943d081a06e941c2bb3ddc12d64930fd6ca5
irustm <jamakrus@gmail.com>
2020-05-26 03:53:11 +0300

    Fixes Log middlware example

--- examples/basic/middlewares/log.middleware.ts
+++ examples/basic/middlewares/log.middleware.ts
@@ -1,18 +1,19 @@
 import { Middleware } from '../../../src/decorator/Middleware.ts';
 import { MiddlewareTarget } from '../../../src/models/middleware-target.ts';
+import { Context } from '../../../src/models/context.ts';
 
 @Middleware(new RegExp('/'))
 export class Log implements MiddlewareTarget {
     date: Date = new Date();
 
-    onPreRequest(request: any, responce: any) {
+    onPreRequest(context: Context) {
         return new Promise((resolve, reject) => {
             this.date = new Date();
             resolve();
         });
     }
 
-    onPostRequest(request: any, responce: any) {
+    onPostRequest(context: Context) {
         return new Promise((resolve, reject) => {
             console.log(new Date().getTime() - this.date.getTime());
             resolve();
