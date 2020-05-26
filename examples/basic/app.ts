b0898ddecdb131be09d90c10cdaee328116b5f30
Rustam <jamakrus@gmail.com>
2020-05-27 02:07:04 +0300

    Added globar error handler, closes #9 (#60)

--- examples/basic/app.ts
+++ examples/basic/app.ts
@@ -3,4 +3,5 @@ import { settings } from './app-settings.ts';
 
 // create application
 const app = new App(settings);
+
 app.listen();
--- examples/default/app.ts
+++ examples/default/app.ts
@@ -1,20 +1,5 @@
-// import { Area, Controller, Get, QueryParam } from '../../mod.ts';
 import { HomeArea } from './areas/home.area.ts';
 import { App } from '../../mod.ts';
-
-// @Controller('/home')
-// export class HomeController {
-
-//     @Get('/query-name')
-//     text(@QueryParam('name') name: string) {
-//         return `Hey! ${name}`;
-//     }
-// }
-
-// @Area({
-//     controllers: [HomeController],
-// })
-// export class HomeArea { }
 
 const app = new App({
     areas: [HomeArea],
