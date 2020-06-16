30b57bbf773b19b31192b23569f07f2cb2ca57e3
irustm <jamakrus@gmail.com>
2020-06-16 21:33:05 +0300

    fix(fmt): openapi test typo

--- openapi/openapi.test.ts
+++ openapi/openapi.test.ts
@@ -1,8 +1,7 @@
-import { assertEquals } from '../src/deps_test.ts';
-import { settings } from '../examples/basic/app-settings.ts';
-import { AlosaurOpenApiBuilder } from './mod.ts';
+import { assertEquals } from "../src/deps_test.ts";
+import { settings } from "../examples/basic/app-settings.ts";
+import { AlosaurOpenApiBuilder } from "./mod.ts";
 const { test } = Deno;
-
 
 /**
  * Test cases
@@ -11,16 +10,16 @@ test({
   name: "[openapi] basic server should generate docs",
   async fn(): Promise<void> {
     const spec = AlosaurOpenApiBuilder.create(settings)
-    .addTitle("Basic Application")
-    .addVersion("1.0.0")
-    .addDescription("Example Alosaur OpenApi generate")
-    .addServer({
-      url: "http://localhost:8000",
-      description: "Local server",
-    }).getSpec();
+      .addTitle("Basic Application")
+      .addVersion("1.0.0")
+      .addDescription("Example Alosaur OpenApi generate")
+      .addServer({
+        url: "http://localhost:8000",
+        description: "Local server",
+      }).getSpec();
 
-    assertEquals(spec.openapi, '3.0.0');
+    assertEquals(spec.openapi, "3.0.0");
 
     // TODO(irustm) add full tests after implement type reference
-  }
-})
+  },
+});
