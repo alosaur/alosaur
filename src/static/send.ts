646a6820b27660b2e044b78a9dc74c721fbf9255
irustm <jamakrus@gmail.com>
2020-08-16 23:34:54 +0300

    fmt

--- src/static/send.ts
+++ src/static/send.ts
@@ -182,9 +182,7 @@ export async function send(
 
   if (!response.headers.has("Content-Type")) {
     const type = contentType(
-      encodingExt !== ""
-        ? extname(basename(path, encodingExt))
-        : extname(path),
+      encodingExt !== "" ? extname(basename(path, encodingExt)) : extname(path),
     );
 
     response.headers.set("Content-Type", type);
