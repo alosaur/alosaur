e3e84041b323c1a3b2e6c592b39958e244dd7ff1
i rustam <irustm@MacBook-Pro-i.local>
2020-08-29 21:10:13 +0300

    fix typo after fmt

--- src/metadata/action.ts
+++ src/metadata/action.ts
@@ -2,8 +2,7 @@ import { RequestMethod } from "../types/request-method.ts";
 
 export interface ActionMetadataArgs {
   type: RequestMethod;
-  object: Object // object of declaration
-  ;
+  object: Object; // object of declaration
   target: Object;
   method: string;
   route?: string | RegExp;
