758f39d88d7870f3faaf058644f7cc7ee47ed966
Ben Gubler <nebrelbug@gmail.com>
2020-09-09 17:55:47 -0600

    Add example of Eta template engine

---
+++ examples/eta/app.ts
@@ -0,0 +1,32 @@
+import { renderFile, configure } from "https://deno.land/x/eta@v1.5.0/mod.ts"
+import { App, Area, Controller, Get, QueryParam, View } from "../../mod.ts"
+
+const viewPath = `${Deno.cwd()}/examples/eta/views/`
+
+@Controller("")
+export class HomeController {
+  @Get("/")
+  text(@QueryParam("name") name: string) {
+    return View("main", { name })
+  }
+}
+
+@Area({
+  controllers: [HomeController]
+})
+export class HomeArea {}
+
+const app = new App({
+  areas: [HomeArea]
+})
+
+// Configure Eta
+configure({ views: viewPath, async: true })
+
+app.useViewRender({
+  type: "eta",
+  basePath: viewPath,
+  getBody: async (path: string, model: Object) => await renderFile(path, model)
+})
+
+app.listen()
