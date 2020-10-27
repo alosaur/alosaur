80c0eb5db0f18fc59f27c6b21c367ae75ca2d107
irustm <jamakrus@gmail.com>
2020-10-27 22:18:31 +0300

    feat: added sse example

---
+++ examples/sse/chat.handler.ts
@@ -0,0 +1,30 @@
+import {
+  isWebSocketCloseEvent,
+  WebSocket,
+} from "https://deno.land/std@0.74.0/ws/mod.ts";
+
+const clients = new Map<number, WebSocket>();
+let clientId = 0;
+function dispatch(msg: string): void {
+  for (const client of clients.values()) {
+    client.send(msg);
+  }
+}
+
+export async function ChatHandler(ws: WebSocket): Promise<void> {
+  const id = ++clientId;
+  clients.set(id, ws);
+  dispatch(`Connected: [${id}]`);
+
+  for await (const msg of ws) {
+    console.log(`msg:${id}`, msg);
+
+    if (typeof msg === "string") {
+      dispatch(`[${id}]: ${msg}`);
+    } else if (isWebSocketCloseEvent(msg)) {
+      clients.delete(id);
+      dispatch(`Closed: [${id}]`);
+      break;
+    }
+  }
+}
---
+++ examples/sse/sse.middlware.ts
@@ -0,0 +1,25 @@
+import { PreRequestMiddleware } from "../../src/models/middleware-target.ts";
+import { Context } from "../../src/models/context.ts";
+import { acceptWebSocket } from "https://deno.land/std@0.74.0/ws/mod.ts";
+import { ChatHandler } from "./chat.handler.ts";
+
+export class SseMiddleware implements PreRequestMiddleware {
+  onPreRequest(context: Context) {
+    const { conn, r: bufReader, w: bufWriter, headers } =
+      context.request.serverRequest;
+    //
+    // acceptWebSocket({
+    //   conn,
+    //   bufReader,
+    //   bufWriter,
+    //   headers,
+    // })
+    //   .then(ChatHandler)
+    //   .catch(async (e) => {
+    //     console.error(`failed to accept websocket: ${e}`);
+    //     await context.request.serverRequest.respond({ status: 400 });
+    //   });
+
+    context.response.setNotRespond();
+  }
+}
