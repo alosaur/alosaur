a1bd330d080e39693afc19f660ec2fc23dcff5f0
irustm <jamakrus@gmail.com>
2020-12-27 21:27:53 +0300

    fix(example): remove engine

--- examples/react/app.ts
+++ examples/react/app.ts
@@ -1,8 +1,8 @@
 import { HomeArea } from "./areas/home.area.ts";
 import { App } from "../../mod.ts";
 import { CoreArea } from "./areas/core.area.ts";
-import { engine } from "./engine.ts";
 import { ViewRenderConfig } from "../../src/models/view-render-config.ts";
+import { getPage } from "./views/app.tsx";
 
 const app = new App({
   areas: [HomeArea, CoreArea],
@@ -13,7 +13,7 @@ app.useViewRender({
   type: "react",
   basePath: `${Deno.cwd()}/examples/react/views/`,
   getBody: async (path: string, model: Object, config: ViewRenderConfig) =>
-    await engine.render(path, model),
+    await getPage(path, model),
 });
 
 app.listen();
