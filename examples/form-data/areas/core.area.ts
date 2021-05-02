1d2891f95251b970c12497c4a84140096e3b23ab
irustm <jamakrus@gmail.com>
2021-05-02 12:25:48 +0300

    fix: append multiple cookie on merge result chore: up deno std 95

--- examples/form-data/areas/core.area.ts
+++ examples/form-data/areas/core.area.ts
@@ -8,8 +8,8 @@ import {
   Post,
 } from "alosaur/mod.ts";
 
-import { FormFile } from "https://deno.land/std@0.93.0/mime/multipart.ts";
-import { move } from "https://deno.land/std@0.93.0/fs/move.ts";
+import { FormFile } from "https://deno.land/std@0.95.0/mime/multipart.ts";
+import { move } from "https://deno.land/std@0.95.0/fs/move.ts";
 
 @Controller()
 export class CoreController {
