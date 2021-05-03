03b1cce7c643abbcc06542588f3407aa079be4b4
irustm <jamakrus@gmail.com>
2021-05-03 21:08:39 +0300

    fix: remove ls

--- e2e/microservice.test.ts
+++ e2e/microservice.test.ts
@@ -9,28 +9,6 @@ const { test } = Deno;
 test({
   name: "microservice app server should response",
   async fn(): Promise<void> {
-    const ls = Deno.run({
-      cmd: ["ls", "./examples/microservice/mservice/"],
-      stdout: "piped",
-      stderr: "inherit",
-    });
-
-    const r = new TextProtoReader(new BufReader(ls.stdout as any));
-    console.log("line", await r.readLine());
-    console.log("line", await r.readLine());
-    console.log("line", await r.readLine());
-
-    const ls2 = Deno.run({
-      cmd: ["ls", "./examples/microservice/app/"],
-      stdout: "piped",
-      stderr: "inherit",
-    });
-
-    const r2 = new TextProtoReader(new BufReader(ls2.stdout as any));
-    console.log("line2", await r2.readLine());
-    console.log("line2", await r2.readLine());
-    console.log("line2", await r2.readLine());
-
     const process1 = await startServer(
       "./examples/microservice/mservice/app.ts",
     );
@@ -49,8 +27,6 @@ test({
     } finally {
       killServer(process1);
       killServer(process2);
-      killServer(ls);
-      killServer(ls2);
     }
   },
 });
