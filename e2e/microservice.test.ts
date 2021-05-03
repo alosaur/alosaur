d76038addf400dba7ebe00913c12f3f1939c1d05
irustm <jamakrus@gmail.com>
2021-05-03 20:50:58 +0300

    feat(test): ms test pathes

--- e2e/microservice.test.ts
+++ e2e/microservice.test.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from "../src/deps_test.ts";
+import {assertEquals, BufReader, TextProtoReader} from "../src/deps_test.ts";
 import { killServer, startServer } from "./test.utils.ts";
 import { delay } from "../examples/_utils/test.utils.ts";
 const { test } = Deno;
@@ -9,9 +9,25 @@ const { test } = Deno;
 test({
   name: "microservice app server should response",
   async fn(): Promise<void> {
-    // const process1 = await startServer(
-    //   "./examples/microservice/mservice/app.ts",
-    // );
+
+    const ls = Deno.run({
+      cmd: ['ls'],
+      stdout: "piped",
+      stderr: "inherit",
+    });
+
+    const decoder = new TextDecoder();
+    const r = new TextProtoReader(new BufReader(ls.stdout as any));
+    console.log('line',  await r.readLine());
+    console.log('line',  await r.readLine());
+    console.log('line',  await r.readLine());
+    console.log('line',  await r.readLine());
+    console.log('line',  await r.readLine());
+
+
+    const process1 = await startServer(
+      "./examples/microservice/mservice/app.ts",
+    );
     const process2 = await startServer("./examples/microservice/app/app.ts");
 
     await delay(100);
@@ -25,8 +41,9 @@ test({
 
       assertEquals(text_r, "Hello world, 10");
     } finally {
-      // killServer(process1);
+      killServer(process1);
       killServer(process2);
+      killServer(ls);
     }
   },
 });
